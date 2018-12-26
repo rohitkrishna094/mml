aab1885dd1ed8b0606c3b286e3e9e285cf0324af
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-12-26 02:44:18 -0500

    fe: added antd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Header />
+        <Footer />
       </div>
     );
   }
