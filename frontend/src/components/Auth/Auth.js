a52e57d1c837b68961c9583f6514731a7006fb0d
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-12-26 14:45:20 -0500

    Added buttons

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Row, Col, Input, Icon, Button } from 'antd';
+import './Auth.css';
 
 const { Content } = Layout;
 export default class Auth extends Component {
   render() {
     return (
-      <Content style={{ padding: '0 50px' }}>
-        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Signup and Signin page</div>
-      </Content>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+        <Row type="flex" justify="space-around" align="middle">
+          <Col span={4} offset={4}>
+            <Input
+              placeholder="Enter your username"
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="Auth_Input"
+            />
+            <Input
+              placeholder="Enter your password"
+              prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="Auth_Input"
+            />
+            <Input
+              placeholder="Confirm your password"
+              prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="Auth_Input"
+            />
+            <Button type="primary" block>
+              Sign Up
+            </Button>
+          </Col>
+          <Col span={4} pull={4}>
+            <Input
+              placeholder="Enter your username"
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="Auth_Input"
+            />
+            <Input
+              placeholder="Enter your password"
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              className="Auth_Input"
+            />
+            <Button type="danger" block>
+              Login
+            </Button>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
