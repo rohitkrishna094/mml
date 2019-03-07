c9c1dd5fc83e450f93021b4986bdb9eb88ad7dc6
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-03-07 13:35:47 -0500

    added profile.js

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Upload, message, Button, Icon } from 'antd';
 
 class Profile extends Component {
   render() {
-    return <div className="">Profile Works</div>;
+    const props = {
+      name: 'file',
+      action:
+        'http://ec2-52-24-8-126.us-west-2.compute.amazonaws.com:8080/candidate/5c1aae39f37a6f35c4d84586/documents/save',
+      headers: {
+        // 'content-type': 'multipart/form-data'
+      },
+      onChange(info) {
+        if (info.file.status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      }
+    };
+    return (
+      <Upload {...props}>
+        <Button>
+          <Icon type="upload" /> Click to Upload
+        </Button>
+      </Upload>
+    );
   }
 }
 
